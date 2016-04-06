f8ec2c8d2082c7572a789fdff36e2599a2e0cbb5
GraceLilac <lovelove8926@naver.com>
2016-04-07 01:17:00 +0900

    create button, botton touch event init

--- ViewControllerJsonPracticeOnlyCode/ViewControllerJsonPracticeOnlyCode/FirstViewController.m
+++ ViewControllerJsonPracticeOnlyCode/ViewControllerJsonPracticeOnlyCode/FirstViewController.m
@@ -10,18 +10,31 @@
 
 @interface FirstViewController ()
 
+@property (nonatomic, strong) UIButton *button;
+
 @end
 
 @implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.button = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 50, 50)]; //버튼을 만든다
+    [self.button setBackgroundColor:[UIColor redColor]]; //버튼의 background color 지정
+    
+    [self.button addTarget:self action:@selector(showCameraRoll:) forControlEvents:UIControlEventTouchUpInside]; //버튼 클릭시 타겟 지정
+
+    [self.view addSubview:self.button]; //현재 뷰의 하위 뷰로 버튼 지정
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)showCameraRoll:(UIButton*)sender
+{
+    NSLog(@"hi");
 }
 
 @end
